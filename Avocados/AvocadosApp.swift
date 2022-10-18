b4368236845e3d7e131ac899470a6e0721342efc
soroushamdg <soroushmpour@gmail.com>
2022-10-18 13:21:32 -0400

    Initial Commit

---
+++ Avocados/AvocadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadosApp.swift
+//  Avocados
+//
+//  Created by Soro on 2022-10-18.
+//
+
+import SwiftUI
+
+@main
+struct AvocadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
