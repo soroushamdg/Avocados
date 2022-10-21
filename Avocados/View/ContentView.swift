008f0014a099331577c2765ebd525a4f27fcfdfb
soroushamdg <soroushmpour@gmail.com>
2022-10-21 13:00:51 -0400

    implemented head view

--- Avocados/View/ContentView.swift
+++ Avocados/View/ContentView.swift
@@ -9,11 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
+        ScrollView(.vertical,showsIndicators: false){
             
-            Text("Avocado Recipes")
+            VStack(alignment: .center,spacing: 20) {
+                ScrollView(.horizontal,showsIndicators: false){
+                    HStack(alignment: .top,spacing: 0) {
+                        HeaderView()
+                    }
+                }
+                
+                
+                VStack(alignment: .center,spacing: 20){
+                    Text("All about Avocados")
+                        .font(.system(.title,design: .serif))
+                        .fontWeight(.bold)
+                        .foregroundColor(Color("ColorGreenAdaptive"))
+                        .padding(8)
+                    
+                    Text("Everything you wanted to know about Avocados")
+                        .font(.system(.body,design: .serif))
+                        .multilineTextAlignment(.center)
+                        .foregroundColor(Color.gray)
+                }
+                .frame(maxWidth: 640)
+                .padding()
+                .padding(.bottom,85)
+            }
         }
-        .padding()
+        .edgesIgnoringSafeArea(.all)
+        .padding(0)
     }
 }
 
---
+++ Avocados/View/HeaderView.swift
@@ -0,0 +1,67 @@
+//
+//  HeaderView.swift
+//  Avocados
+//
+//  Created by Soro on 2022-10-21.
+//
+
+import SwiftUI
+
+struct HeaderView: View {
+    
+    @State private var showHeadline: Bool = false
+    
+    var slideInAnimation: Animation {
+        Animation.spring(response: 1.5, dampingFraction: 0.5,blendDuration: 0.5)
+            .speed(1)
+            .delay(0.25)
+    }
+    
+    var body: some View {
+
+        ZStack{
+            Image("avocado-slice-1")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+            
+            HStack(alignment: .top,spacing: 0){
+                Rectangle()
+                    .fill(Color("ColorGreenLight"))
+                    .frame(width: 4)
+                
+                VStack(alignment: .leading, spacing: 6){
+                    Text("Avacado")
+                        .font(.system(.title,design: .serif))
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .shadow(radius: 3)
+                    
+                    Text("Avocados are a powerhouse ingredient at any meal for anyone.")
+                        .font(.footnote)
+                        .lineLimit(2)
+                        .multilineTextAlignment(.leading)
+                        .foregroundColor(.white)
+                        .shadow(radius: 3)
+                }
+                .padding(.vertical,0)
+                .padding(.horizontal,20)
+                .frame(width: 281,height: 105)
+                .background(Color("ColorBlackTransparentLight"))
+            }
+            .frame(width: 285,height: 105,alignment: .center)
+            .offset(x:-66, y: showHeadline ? 75 : 220)
+            .animation(slideInAnimation)
+            .onAppear(){
+                showHeadline.toggle()
+            }
+        }
+        .frame(width: 480,height: 320,alignment: .center)
+    }
+}
+
+struct HeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        HeaderView()
+            .previewLayout(.sizeThatFits)
+    }
+}
